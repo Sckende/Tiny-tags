5b8d5e1e234251c56324fbbdbaa824333fdea911
Sckende <clrccljuhasz@gmail.com>
2018-01-15 20:54:46 -0500

    script de merde

--- script Tiny.R
+++ script Tiny.R
@@ -1,12 +1,41 @@
 getwd()
-setwd("C:/Users/Toya/Desktop/TinyTag")
-data<-read.table("data.txt", sep="\t", dec=",",h=T)
-data
-summary(data)
-data$date
-data$date2<-substr(data$date,1,10)
-data$heure<-substr(data$date,12,19)
+install.packages("xlsx")
+#require(readxl)
+setwd("/home/claire/Documents/GitHub/Tiny-tags/2017")
 
-summary(data)
+d<-read.table("ALL_DATA.csv", sep=";", dec=".",h=T)
+dd <- read.table("NL-4_TM15_181.csv", sep = "\t", h = T)
+head(d)
+summary(d)
 
-plot(data)
+# Transformation de la variable DATE
+d$DATE
+d$DATE2 <- strptime(d$DATE, format = "%d-%m-%Y %H:%M:%S")
+d$DATE3 <- strptime(d$DATE, format = "%d-%m-%Y %H:%M:%S", tz = "EDT" )
+
+l <- split(d, d$TT)
+names(l)
+k <- 1:(3*24*60*60)
+plot(l[[9]]$DATE2[k], l[[9]]$TEMP[k], type = "l")
+lapply(l, function(x){
+  plot(x$DATE2, x$TEMP, type = "l", main = x$TT[1])
+}
+)
+
+dd$TEMP <- gsub(" °C", "", dd$TEMP)
+dd$TEMP <- as.numeric(gsub(",", ".", dd$TEMP))
+dd$DATE2 <- strptime(dd$DATE, format = "%d-%m-%Y %H:%M:%S")
+summary(dd)
+
+k <- 1:1500
+plot(dd$DATE2[k], dd$TEMP[k], type = "l")
+
+
+setwd("/home/claire/Téléchargements")
+read_xls("NL12.xls" )
+list.files()
+
+install.packages("gdata")
+library(gdata)
+?read.xls
+read.xls(xls = "/home/claire/Documents/GitHub/Tiny-tags/2017/NL-11.xls",skip="50")
