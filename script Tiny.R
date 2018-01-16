688d9d9e9134f9ff14e6af9ee344fea0318a10b8
Nicolas Lecomte <nicolas@MacBook-Air-de-Nicolas-2.local>
2018-01-16 17:16:34 -0500

    Annotations

--- script Tiny.R
+++ script Tiny.R
@@ -40,34 +40,8 @@ library(gdata)
 ?read.xls
 read.xls(xls = "/home/claire/Documents/GitHub/Tiny-tags/2017/NL-11.xls",skip="50")
 
+#Manipulation données 2017
 setwd("/Users/nicolas/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - RAW DATA/GOOSE-TinyTags/TinyTags_2017/txt")
-d <- read.table("NL-6-MESWAT-187.txt", sep = "\t", dec = ",", h = T)
-head(d)
-d$DATE2 <- strptime(d$DATE, format = "%d-%m-%Y %H:%M:%S")
-d$SONDE <- "NL-6"
-summary(d)
-plot(d$DATE2, d$TEMP, type = "l", main = d$SONDE[1])
-
-d <- read.table("NL-11.txt", sep = "\t", dec = ",", h = T)
-head(d)
-d$DATE2 <- strptime(d$DATE, format = "%d-%m-%Y %H:%M:%S")
-d$SONDE <- "NL-11"
-summary(d)
-plot(d$DATE2, d$TEMP, type = "l", main = d$SONDE[1])
-
-d <- read.table("NL-2.txt", sep = "\t", dec = ",", h = T)
-head(d)
-d$DATE2 <- strptime(d$DATE, format = "%d-%m-%Y %H:%M:%S")
-d$SONDE <- "NL-2"
-summary(d)
-plot(d$DATE2, d$TEMP, type = "l", main = d$SONDE[1])
-
-d <- read.table("NL-5.txt", sep = "\t", dec = ",", h = T)
-head(d)
-d$DATE2 <- strptime(d$DATE, format = "%d-%m-%Y %H:%M:%S")
-d$SONDE <- "NL-5"
-summary(d)
-plot(d$DATE2, d$TEMP, type = "l", main = d$SONDE[1])
 
 t <- list.files()
 for (i in t) {
@@ -81,18 +55,8 @@ for (i in t) {
   write.csv(d, file = paste(d$SONDE[1],"bis.txt"))
 }
 
-setwd("/Users/nicolas/Desktop/TT 2016?")
-list.files()
-b <- read.table("nl2.txt", sep = "\t", dec = ",", h = T)
-b <- read.csv("nl13.csv", sep = ";", dec = ",", h = T)
-head(b)
-b$DATE2 <- strptime(b$DATE, format = "%d-%m-%Y %H:%M:%S")
-b$SONDE <- "NL-2"
-b$SONDE <- "NL-13"
-plot(b$DATE2, b$TEMP, main = b$SONDE[1], type = "l")
-
+#Manipulation données 2016
 setwd("/Users/nicolas/OneDrive - Université de Moncton/Doc doc doc/Ph.D. - RAW DATA/GOOSE-TinyTags/TinyTags_2016/txt")
-
 t <- list.files()
 for (i in t) {
   d <- read.table(i, sep = "\t", dec = ",", h = T)
